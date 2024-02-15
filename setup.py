d23da3c62b2f9b90f16a654447e3d6e405fcaeb1
ronilpatil <ronilpatil2000@gmail.com>
2024-02-15 19:07:59 +0530

    template initilized

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='e2e machine learning workflow [tech stack: git, dvc, dvclive, mlflow, fastapi, pytest, github actions, docker]',
+    author='ronilpatil',
+    license='MIT',
+)
