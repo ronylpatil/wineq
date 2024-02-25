ed43e6f1dd5b75edb5e18824238ee37d02f17c98
ronilpatil <ronilpatil2000@gmail.com>
2024-02-25 10:23:38 +0530

    minor update

--- service/api.py
+++ service/api.py
@@ -10,7 +10,7 @@ api_keys = ['xgb0fws23']  # This is encrypted in the database
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl = 'token')  # use token authentication
 
-def api_key_auth(api_key: str = Depends(oauth2_scheme)) :
+def api_key_auth(api_key: str = Depends(oauth2_scheme)) -> None :
      if api_key not in api_keys :
           raise HTTPException(
                status_code = status.HTTP_401_UNAUTHORIZED,
